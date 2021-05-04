2f37698802a79fd4dad2be0b1c3a196da9688d76
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-05-04 15:04:23 +0100

    Project setup

---
+++ StormTracker/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  StormTracker
+//
+//  Created by waheedCodes on 04/05/2021.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+
+}
