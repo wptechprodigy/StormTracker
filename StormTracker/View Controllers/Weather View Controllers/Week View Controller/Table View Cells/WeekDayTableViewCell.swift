091d36bba33995a0094eb7f282c7ef85a93d8283
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-05-14 10:53:04 +0100

    add ui elects to the week day table view cell

--- StormTracker/View Controllers/Weather View Controllers/Week View Controller/Table View Cells/WeekDayTableViewCell.swift
+++ StormTracker/View Controllers/Weather View Controllers/Week View Controller/Table View Cells/WeekDayTableViewCell.swift
@@ -15,6 +15,43 @@ class WeekDayTableViewCell: UITableViewCell {
         return String(describing: self)
     }
     
+    // MARK: - Properties
+    
+    @IBOutlet weak var dayLabel: UILabel! {
+        didSet {
+            dayLabel.textColor = StormTracker.Color.baseTextColor
+            dayLabel.font = StormTracker.Fonts.heavyLarge
+        }
+    }
+    
+    @IBOutlet weak var dateLabel: UILabel! {
+        didSet {
+            dateLabel.textColor = .black
+            dateLabel.font = StormTracker.Fonts.lightRegular
+        }
+    }
+    
+    @IBOutlet weak var windSpeedLabel: UILabel! {
+        didSet {
+            windSpeedLabel.textColor = .black
+            windSpeedLabel.font = StormTracker.Fonts.lightSmall
+        }
+    }
+    
+    @IBOutlet weak var temperatureLabel: UILabel! {
+        didSet {
+            temperatureLabel.textColor = .black
+            temperatureLabel.font = StormTracker.Fonts.lightSmall
+        }
+    }
+    
+    @IBOutlet weak var iconImageView: UIImageView! {
+        didSet {
+            iconImageView.contentMode = .scaleAspectFit
+            iconImageView.tintColor = StormTracker.Color.baseTintColor
+        }
+    }
+    
     // MARK: - Initialization
     
     override func awakeFromNib() {
