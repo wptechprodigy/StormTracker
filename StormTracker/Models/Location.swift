4f4320bad76d4783c1b288dbcc18df95d35db3b1
Waheed Afolabi <waheed.freelancer16@gmail.com>
2021-05-20 12:22:23 +0100

    add location model to prepare ground for making the Root View Model independent on the Core Location framework

---
+++ StormTracker/Models/Location.swift
@@ -0,0 +1,17 @@
+//
+//  Location.swift
+//  StormTracker
+//
+//  Created by waheedCodes on 20/05/2021.
+//
+
+import Foundation
+
+struct Location {
+    
+    // MARK: - Properties
+    
+    let latitude: Double
+    let longitude: Double
+    
+}
